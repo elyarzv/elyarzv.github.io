db55f56940506049ba408e31032c45dda453ebea
Elyar Zavvari <elyarzavvari@Elyars-MacBook-Air-2.local>
2024-12-14 01:41:29 -0800

    phone

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
 
     // Adjust scale based on scroll position (control the divisor to tweak sensitivity)
-    const scaleValue = 1 + scrollY / 350;
-    const opacityValue = Math.max(1.5 - scrollY / 100, 0);
+    const scaleValue = 1 + scrollY / 500;
+    const opacityValue = Math.max(1.5 - scrollY / 300, 0);
 
     // Apply the scaling effect
     zoomText.style.transform = `translate(-50%, -50%) scale(${scaleValue})`;
